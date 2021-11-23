ccacc6aae4cf74a08c63bad9c3e37ca2e5be15a4
artom web <artomweb@gmail.com>
2021-11-23 22:35:01 +0000

    texture

--- video.js
+++ video.js
@@ -2,7 +2,12 @@ let car;
 let tables = [];
 let pointsCsv = [];
 
+let realSF = 520 / 111;
+let plateHeight = 100;
+let myFont;
+
 function preload() {
+    myFont = loadFont("CharlesWright-Bold.ttf");
     let csvFiles = ["points/point-3.csv", "points/point-2.csv", "points/point-1.csv", "points/point-0.csv"];
 
     for (let file of csvFiles) {
@@ -10,13 +15,15 @@ function preload() {
     }
 }
 
+let plateGraphic;
 let plate;
 
 function setup() {
     car = createVideo("media/pexels-cottonbro-7508263.mp4");
-    canvas = createCanvas(1000, 527);
+    let canvas = createCanvas(1000, 527, WEBGL);
     canvas.parent("sketch");
-    plate = createGraphics(100, 100);
+    plateGraphic = createGraphics(800, 500);
+    // plateGraphic.parent("sketch");
     // console.log(car);
     // car.loop();
     car.hide();
@@ -32,42 +39,139 @@ function setup() {
     }
     console.log(pointsCsv);
 
+    plate = new generatePlate(plateGraphic.width / 2, plateGraphic.height / 2, plateHeight, realSF);
+
+    plateGraphic.scale(3);
     // let srcCorners = [450, 213.5, 550, 313.5, 550, 313.5, 450, 313.5];
 }
 
 function draw() {
-    background("white");
-
-    let currFrame = floor(car.time() * 25);
-    if (currFrame > 300) return car.pause();
+    background(100);
+    push();
+    translate(-width / 2, -height / 2, 0);
     image(car, 0, 0, width, height);
+    fill("white");
+    noStroke();
+    quad(38, 31, 86, 20, 69, 63, 30, 76);
+    pop();
+    let currFrame = floor(car.time() * 25);
+    // if (currFrame > 300) return car.pause();
+    // image(car, 0, 0, width, height);
     // console.log(currFrame);
 
-    for (let i = 0; i < pointsCsv.length; i++) {
-        let thisP = pointsCsv[i][currFrame];
-        // console.log(thisP);
-        if (i == 3) {
-            fill("red");
-        } else {
-            fill("blue");
-        }
-        circle(thisP.x, thisP.y, 10);
-    }
+    plateGraphic.push();
+    plateGraphic.translate(-100, -150, 0);
 
-    let srcCorners = [450, 213.5, 550, 313.5, 550, 313.5, 450, 313.5];
-    let dstCorners = [pointsCsv[0][0].x, pointsCsv[0][0].y, pointsCsv[1][0].x, pointsCsv[1][0].y, pointsCsv[2][0].x, pointsCsv[2][0].y, pointsCsv[3][0].x, pointsCsv[3][0].y];
+    plateGraphic.background(255);
+    // plateGraphic.scale(2);
+
+    plateGraphic.noStroke();
+
+    plateGraphic.rectMode(CENTER);
+
+    // plateGraphic.fill("#333745");
+    // plateGraphic.rect(plate.pos.x + 5, plate.pos.y + 5, plate.plateWidth, plate.plateHeight);
+
+    plateGraphic.fill("#edf6f9");
+    plateGraphic.rect(plate.pos.x, plate.pos.y, plate.plateWidth, plate.plateHeight);
+
+    plateGraphic.textFont(myFont);
+    plateGraphic.fill("black");
+    plateGraphic.textSize(80);
+
+    plateGraphic.push();
+
+    plateGraphic.translate(plate.pos.x - 175, plate.pos.y - 10);
+
+    for (let i = 0; i < plate.chars.length; i++) {
+        plateGraphic.textAlign(CENTER, CENTER);
+
+        plateGraphic.text(plate.chars[i], 0, 0);
+
+        if (i == 3) {
+            plateGraphic.translate(90, 0);
+        } else {
+            plateGraphic.translate(50, 0);
+        }
+    }
+    plateGraphic.pop();
+    plateGraphic.pop();
+
+    // fill("white");
+
+    // plateGraphic.textSize(50);
+    // plateGraphic.fill("red");
+    // plateGraphic.rect(0, 0, 100, 50);
+    // fill("white");
+    // push();
+    // // translate(0, 200);
+    // // rotateX(0.5);
+    noStroke();
+    // // plane(200);
+    // rect(0, 0, 100, 50);
+    // pop();
+    texture(plateGraphic);
+    // beginShape();
+    translate(-width / 2, -height / 2, 0);
+
+    quad(
+        pointsCsv[0][currFrame].x,
+        pointsCsv[0][currFrame].y,
+        pointsCsv[1][currFrame].x,
+        pointsCsv[1][currFrame].y,
+        pointsCsv[2][currFrame].x,
+        pointsCsv[2][currFrame].y,
+        pointsCsv[3][currFrame].x,
+        pointsCsv[3][currFrame].y
+    );
+
+    // for (let i = 0; i < pointsCsv.length; i++) {
+    //     let thisP = pointsCsv[i][currFrame];
+    //     // console.log(thisP);
+    //     // if (i == 0) {
+    //     //     fill("red");
+    //     // } else {
+    //     // fill("blue");
+    //     // }
+    //     circle(thisP.x, thisP.y, 10);
+    //     vertex(thisP.x, thisP.y);
+    // }
+
+    // endShape(CLOSE);
+
+    // plateGraphic.fill("orange");
+
+    //
+
+    // let srcCorners = [0, 0, width, 0, width, height, 0, height];
+    // let dstCorners = [
+    //     pointsCsv[0][currFrame].x,
+    //     pointsCsv[0][currFrame].y,
+    //     pointsCsv[1][currFrame].x,
+    //     pointsCsv[1][currFrame].y,
+    //     pointsCsv[2][currFrame].x,
+    //     pointsCsv[2][currFrame].y,
+    //     pointsCsv[3][currFrame].x,
+    //     pointsCsv[3][currFrame].y,
+    // ];
     // console.log(dstCorners);
-    var perspT = PerspT(srcCorners, dstCorners);
-    var mat = perspT.coeffsInv;
+    // var perspT = PerspT(srcCorners, dstCorners);
+    // var mat = perspT.coeffsInv;
+    // console.log("matrix(" + mat.slice(0, 6).join(", ") + ")");
+
+    // plate.style("transform", "matrix(" + mat.join(", ") + ")");
+    // let z = map(mouseY, 0, windowHeight, -1000, 250);
+    // div.style("transform", "translateZ(" + z + "px)");
+
     // console.log(mat);
 
-    plate.push();
-    plate.applyMatrix(mat[0], mat[1], mat[2], mat[3], mat[4], mat[6]);
-    plate.fill("blue");
-    plate.rectMode(CENTER);
-    plate.rect(0, 0, plate.width, plate.height);
-    image(plate, width / 2, height / 2);
-    plate.pop();
+    // plate.push();
+    // plate.applyMatrix(1, mat[1], mat[3], 1, 0, 0);
+    // plate.fill("blue");
+    // plate.rectMode(CENTER);
+    // plate.rect(0, 0, 200, 75);
+    // image(plate, 0, 0);
+    // plate.pop();
 }
 
 function mousePressed() {
